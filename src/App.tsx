d19ea5c0a8b443867c09bdf85220883db531f9d5
Nguyen Vo Nghiem Minh <nguyenvonghiemminh156@gmail.com>
2025-09-26 11:11:46 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import TabsContent from "./components/tabs.content";
+import UsersTable from "./components/user.table";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <TabsContent />
+    </>
+  );
+}
+
+export default App;
